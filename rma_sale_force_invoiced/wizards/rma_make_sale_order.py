cd130877d62196a9ff625596136b3eb87d183b81
chafique.delli <chafique.delli@akretion.com>
2024-07-04 00:12:23 +0200

    [IMP] create sale order from rma group

--- rma_sale_force_invoiced/wizards/rma_make_sale_order.py
+++ rma_sale_force_invoiced/wizards/rma_make_sale_order.py
@@ -4,8 +4,8 @@
 from odoo import api, models
 
 
-class RmaLineMakeSaleOrder(models.TransientModel):
-    _inherit = "rma.order.line.make.sale.order"
+class RmaMakeSaleOrder(models.TransientModel):
+    _inherit = "rma.make.sale.order"
 
     @api.model
     def _prepare_sale_order(self, line):
